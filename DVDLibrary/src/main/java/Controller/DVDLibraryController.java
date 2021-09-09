754d831c543469a04efed7155b0266877d3b33b9
AustinGraham28 <89654150+AustinGraham28@users.noreply.github.com>
2021-09-09 15:21:52 -0400

    Add files via upload

--- DVDLibrary/src/main/java/Controller/DVDLibraryController.java
+++ DVDLibrary/src/main/java/Controller/DVDLibraryController.java
@@ -3,12 +3,112 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Controller;
+package com.sg.dvdlibrary.controller;
+
+import com.sg.dvdlibrary.dao.DVDLibraryDao;
+import com.sg.dvdlibrary.dao.DVDLibraryDaoException;
+import com.sg.dvdlibrary.dto.DVD;
+import com.sg.dvdlibrary.ui.DVDLibraryView;
+import com.sg.dvdlibrary.ui.UserIO;
+import com.sg.dvdlibrary.ui.UserIOConsoleImpl;
+import java.util.List;
 
 /**
  *
- * @author Noah McElroy
+ * @author agrah
  */
 public class DVDLibraryController {
     
+    //private ClassRosterView view = new ClassRosterView();
+    //private ClassRosterDao dao = new ClassRosterDaoFileImpl();
+    private DVDLibraryView view;
+    private DVDLibraryDao dao;
+    private UserIO io = new UserIOConsoleImpl();
+    
+    public DVDLibraryController(DVDLibraryDao daoIn, DVDLibraryView viewIn){
+        this.dao = daoIn;
+        this.view = viewIn;
+    }
+    
+    public void run(){
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        
+        try{
+            while(keepGoing){
+
+                menuSelection = getMenuSelection();
+
+                switch(menuSelection){
+                    case 1: 
+                        createDVD();                       
+                        break;
+                    case 2:
+                        removeDVD();
+                        break;
+                    case 3:
+                        editDVD();
+                        break;
+                    case 4:
+                        listDVDs();
+                        break;
+                    case 5:
+                        viewDVD(); //this searches for and displays dvd info
+                                   //fulfills requirements 5. and 6.
+                        break;
+                    case 6:
+                        keepGoing = false;
+                        break;
+                    default:
+                        unknownCommand();
+                }
+            }
+            exitMessage();
+        }catch(DVDLibraryDaoException e){
+            view.displayErrorMessage(e.getMessage());
+        }
+    }
+    
+    private int getMenuSelection(){
+        return view.printMenuAndGetSelection();
+    }
+    
+    private void createDVD() throws DVDLibraryDaoException {
+        view.displayCreateDVDBanner();
+        DVD newDVD = view.getNewDVD();
+        dao.AddDVD(newDVD.getTitle(), newDVD);
+        view.displaySuccessBanner();
+    }
+    
+    private void removeDVD() throws DVDLibraryDaoException {
+        view.displayRemoveDVDBanner();
+        String title = view.getDVDChoice();
+        DVD removedDVD = dao.RemoveDVD(title);
+        view.displayRemoveResult(removedDVD);
+    }
+
+    private void editDVD() throws DVDLibraryDaoException {
+        
+    }
+        
+    private void listDVDs() throws DVDLibraryDaoException {
+        view.displayDisplayAllBanner();
+        List<DVD> dVDList = dao.ListAllDVD();
+        view.displayDVDList(dVDList);
+    }
+    
+    private void viewDVD() throws DVDLibraryDaoException {
+        view.displayDisplayAllBanner();
+        String title = view.getDVDChoice();
+        DVD dvd = dao.DisplayDVDDetails(title);
+        view.displayDVD(dvd);
+    }
+    
+    private void unknownCommand(){
+        view.displayUnknownCommandBanner();
+    }
+    
+    private void exitMessage(){
+        view.displayExitBanner();
+    }
 }
