e412f13c11274a4a94f6d59c49a44061f2df2d6e
NLRUmbra <56780974+NLRUmbra@users.noreply.github.com>
2021-09-09 14:56:07 -0400

    Update DVDLibraryDaoFileImpl.java

--- DVDLibrary/src/main/java/Dao/DVDLibraryDaoFileImpl.java
+++ DVDLibrary/src/main/java/Dao/DVDLibraryDaoFileImpl.java
@@ -5,10 +5,139 @@
  */
 package Dao;
 
+import DTO.DVD;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  *
  * @author Noah McElroy
  */
-public class DVDLibraryDaoFileImpl {
+public class DVDLibraryDaoFileImpl implements DVDLibraryDao {
+    Map<String, DVD> dvds = new HashMap<>();
+    public static final String DVDCollection_FILE = "DVDCollection.txt";
+    public static final String DELIMITER = "::";
+
+    @Override
+    public DVD AddDVD(String title, DVD dvd) throws DVDLibraryDaoException {
+            loadDVDCollection();
+            DVD newDVD = dvds.put(title, dvd);
+            writeDVDCollection();
+            return newDVD;
+    }
+
+    @Override
+    public DVD RemoveDVD(String title) throws DVDLibraryDaoException {
+            loadDVDCollection();
+            DVD removedDVD = dvds.remove(title);
+            writeDVDCollection();
+            return removedDVD;
+    }
+
+    @Override
+    public DVD EditDVD(String title) throws DVDLibraryDaoException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public DVD DisplayDVDDetails(String title) throws DVDLibraryDaoException {
+        loadDVDCollection();
+        return dvds.get(title);
+    }
+
+    @Override
+    public List<DVD> ListAllDVD() throws DVDLibraryDaoException {
+        loadDVDCollection();
+        return new ArrayList(dvds.values());
+    }
+    private DVD unmarshallDVD(String DVDAsText){
+  
+        String[] DVDTokens = DVDAsText.split(DELIMITER);
+        DVD DVDFromFile = new DVD(DVDTokens[0],Integer.parseInt(DVDTokens[1]),Integer.parseInt(DVDTokens[2]),Integer.parseInt(DVDTokens[3]),DVDTokens[4],DVDTokens[5],DVDTokens[6],DVDTokens[7]);
+        return DVDFromFile;
     
+    }
+    private String marshallDVD(DVD aDVD){
+
+        String DVDAsText = aDVD.getTitle() + DELIMITER;
+
+        DVDAsText += aDVD.getMonth() + DELIMITER;
+
+        DVDAsText += aDVD.getDay() + DELIMITER;
+
+        DVDAsText += aDVD.getYear() + DELIMITER;
+        DVDAsText += aDVD.getMPAARating() + DELIMITER;
+        DVDAsText += aDVD.getDirectorName()+ DELIMITER;
+        DVDAsText += aDVD.getStudio()+ DELIMITER;
+        DVDAsText += aDVD.getUserRating();
+
+    
+        return DVDAsText;
+    } 
+    private void loadDVDCollection() throws DVDLibraryDaoException {
+        Scanner scanner;
+
+        try {
+            // Create Scanner for reading the file
+            scanner = new Scanner(
+                    new BufferedReader(
+                            new FileReader(DVDCollection_FILE)));
+        } catch (FileNotFoundException e) {
+            throw new DVDLibraryDaoException(
+                    "-_- Could not load dvd data into memory.", e);
+        }
+        // currentLine holds the most recent line read from the file
+        String currentLine;
+        // currentDVD holds the most recent student unmarshalled
+        DVD currentDVD;
+        // Go through DVDCollection_FILE line by line, decoding each line into a 
+        // Student object by calling the unmarshallStudent method.
+        // Process while we have more lines in the file
+        while (scanner.hasNextLine()) {
+            // get the next line in the file
+            currentLine = scanner.nextLine();
+            // unmarshall the line into a DVD
+            currentDVD = unmarshallDVD(currentLine);
+
+            // We are going to use the title as the map key for our DVD object.
+            // Put currentDVD into the map using title as the key
+            dvds.put(currentDVD.getTitle(), currentDVD);
+        }
+        // close scanner
+        scanner.close();
+    }
+    private void writeDVDCollection() throws DVDLibraryDaoException {
+  
+        PrintWriter out;
+
+        try {
+            out = new PrintWriter(new FileWriter(DVDCollection_FILE));
+        } catch (IOException e) {
+            throw new DVDLibraryDaoException(
+                    "Could not save dvd data.", e);
+        }
+
+    
+        String DVDAsText;
+        List<DVD> DVDList = this.ListAllDVD();
+        for (DVD currentDVD : DVDList) {
+            // turn a DVD into a String
+            DVDAsText = marshallDVD(currentDVD);
+            // write the DVD object to the file
+            out.println(DVDAsText);
+            // force PrintWriter to write line to the file
+            out.flush();
+        }
+        // Clean up
+        out.close();
+    }
 }
