3b6b1f03c09110722942cd094a46b55da90e9919
NLRUmbra <56780974+NLRUmbra@users.noreply.github.com>
2021-09-09 14:29:36 -0400

    Update DVD.java

--- DVDLibrary/src/main/java/DTO/DVD.java
+++ DVDLibrary/src/main/java/DTO/DVD.java
@@ -10,5 +10,91 @@ package DTO;
  * @author Noah McElroy
  */
 public class DVD {
-    
+    private String title;
+    private int month;
+    private int day;
+    private int year;
+    private String MPAARating;
+    private String directorName;
+    private String Studio;
+    private String userRating;
+
+    public DVD(String title, int month, int day, int year, String MPAARating, String directorName, String Studio, String userRating) {
+        this.title = title;
+        this.month = month;
+        this.day = day;
+        this.year = year;
+        this.MPAARating = MPAARating;
+        this.directorName = directorName;
+        this.Studio = Studio;
+        this.userRating = userRating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMPAARating() {
+        return MPAARating;
+    }
+
+    public void setMPAARating(String MPAARating) {
+        this.MPAARating = MPAARating;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+
+    public String getStudio() {
+        return Studio;
+    }
+
+    public void setStudio(String Studio) {
+        this.Studio = Studio;
+    }
+
+    public String getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(String userRating) {
+        this.userRating = userRating;
+    }
+
+    public DVD(String title) {
+        this.title = title;
+    }
 }
